bd93c6e6933d3bde2a7a7304408349c210bf70b8
Amr69130 <amrouchenorris@gmail.com>
2025-02-27 22:03:20 +0100

    fix form issues

--- choixBoisson.php
+++ choixBoisson.php
@@ -1,2 +1,33 @@
-<?php
-require_once('data.php');
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require_once('data.php');
+    ?>
+
+    <form action="confirmBoisson.php" method="POST">
+
+        <select name="boisson">
+            <?php
+            foreach ($cafes as $info) {
+                echo "<option value='{$info['name']}'>{$info['name']} - {$info['price']}€</option>";
+            }
+
+            ?>
+        </select>
+        <button type="submit">VALIDER COMMANDE</button>
+
+    </form>
+
+
+
+</body>
+
+</html>
