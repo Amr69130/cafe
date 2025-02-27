b993452f6d0cccaf87e988f242ad9ab4ecce6aae
Amr69130 <amrouchenorris@gmail.com>
2025-02-27 15:25:43 +0100

    initial commit

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+$cafes = [
+
+    "Expresso" =>    [
+        "name" => "Expresso",
+        "price" => 0.5,
+        "add_sugar" => true
+    ],
+
+    "Lungo" =>    [
+        "name" => "Lungo",
+        "price" => 1,
+        "add_sugar" => true
+    ],
+
+    "Cappuccino" =>    [
+        "name" => "Cappuccino",
+        "price" => 1.20,
+        "add_sugar" => true
+    ]
+];
