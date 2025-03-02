3842e3b02b73c344c0d2a5e0a8a3ffe2ab40f338
Amr69130 <amrouchenorris@gmail.com>
2025-03-02 13:03:14 +0100

    add form sugar

--- choixSucre.php
+++ choixSucre.php
@@ -1,2 +1,16 @@
 <?php
 require_once('data.php');
+
+var_dump($_GET); ?>
+
+<form action="confirmCommande.php?boisson= .$cafe['name]" method="POST">
+    <h1>CHOISIR NOMBRE DE SUCRE (1 à 5)</h1>
+    <input type="number" min="0" max="5">
+
+
+
+
+
+
+    <button type="submit">VALIDER COMMANDE</button>
+</form>
